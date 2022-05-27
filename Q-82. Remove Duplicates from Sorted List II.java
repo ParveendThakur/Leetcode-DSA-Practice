84a3c2792f3001181a630c3074d09af58de8e658
ParveendThaku <parveend1234@gmail.com>
2022-05-27 14:13:40 +0530

    add the var

--- Q-82. Remove Duplicates from Sorted List II.java
+++ Q-82. Remove Duplicates from Sorted List II.java
@@ -9,3 +9,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
