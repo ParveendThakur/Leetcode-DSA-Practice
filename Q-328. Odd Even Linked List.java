189695cf9eb8ef34e9d38e229a5b176c886d1dbc
ParveendThaku <parveend1234@gmail.com>
2022-06-02 19:39:02 +0530

    all var add

--- Q-328. Odd Even Linked List.java
+++ Q-328. Odd Even Linked List.java
@@ -11,6 +11,9 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        
-    }
-}
+        ListNode odd = new ListNode(0);
+        ListNode even = new ListNode(1);
+        ListNode p1 = odd;
+        ListNode p2 = even;
+        ListNode ptr = head;
+        boolean flag = true;
